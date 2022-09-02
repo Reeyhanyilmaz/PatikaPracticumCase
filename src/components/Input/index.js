25f1c56742a1fcbd979319d3da87d876f967323c
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-02 19:56:20 +0300

    add Input comp content and request

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import { Input } from '@chakra-ui/react'
+import React, { useState } from "react";
+import { Input, Button } from "@chakra-ui/react";
+import axios from "axios";
 
-function InputArea() {
+function InputArea({ fetchData }) {
+  const [inputValue, setInputValue] = useState("Yapılacak:");
+
+  const fetchPost = async () => {
+    const { data } = await axios.post(
+      `https://6311aeb7f5cba498da835aac.mockapi.io/todos`,
+      {
+        content: inputValue,
+        isCompleted: false,
+      }
+    );
+    console.log("data.content", data);
+    return data;
+  };
+
+  const handleClick = (e) => {
+    if (inputValue === "") {
+      alert("Lütfen todo giriniz");
+    } else {
+      e.preventDefault();
+      fetchPost();
+      fetchData();
+      setInputValue("Yapılacak:");
+    }
+  };
+
   return (
-    <div>
-        <Input placeholder='todos...' />
+    <div style={{display: "flex", flexDirection: "row"}}>
+      <Input
+        placeholder="todos..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <Button type="submit" onClick={handleClick}>
+        Ekle
+      </Button>
     </div>
-  )
+  );
 }
 
 export default InputArea;
