b1b36c5f9e19d5f14682dd36dec521c8abc910de
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-03 10:13:19 +0300

    add toggle switch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import "./App.css";
 import Header from "./components/Header";
-import InputArea from "./components/Input";
-import Todos from "./components/Todos"
+import Todos from "./components/Todos";
 import { ChakraProvider } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { DarkModeToggle } from "@anatoliygatt/dark-mode-toggle";
+import { clsx } from "clsx";
 
 function App() {
+  const [mode, setMode] = useState("dark");
+
   return (
-    <div className="App">
-      <ChakraProvider>
-        <Header />
-        <InputArea />
-        <Todos />
-      </ChakraProvider>
+    <div
+      className={clsx("App", {
+        dark: mode === "dark",
+      })}
+    >
+      <div className="container">
+        <DarkModeToggle
+          mode={mode}
+          dark="Dark"
+          light="Light"
+          size="md"
+          inactiveTrackColor="#e2e8f0"
+          inactiveTrackColorOnHover="#f8fafc"
+          inactiveTrackColorOnActive="#cbd5e1"
+          activeTrackColor="#334155"
+          activeTrackColorOnHover="#1e293b"
+          activeTrackColorOnActive="#0f172a"
+          inactiveThumbColor="#1e293b"
+          activeThumbColor="#e2e8f0"
+          onChange={(mode) => {
+            setMode(mode);
+          }}
+        />
+        <ChakraProvider>
+          {/* <Toggle /> */}
+          <Header />
+          <Todos />
+        </ChakraProvider>
+      </div>
     </div>
   );
 }
