cddb8adfeefc69a438a8231cf0f6a7f2fa484a27
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-05 11:22:18 +0300

    add mode localStorage and toggle styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./components/Header";
 import Todos from "./components/Todos";
 import { ChakraProvider } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DarkModeToggle } from "@anatoliygatt/dark-mode-toggle";
 import { clsx } from "clsx";
 
 function App() {
-  const [mode, setMode] = useState("dark");
+  const [mode, setMode] = useState(localStorage.getItem("mode"));
+
+  useEffect(() => {
+    localStorage.setItem("mode", mode);
+  }, [mode]);
 
   return (
     <div
@@ -16,25 +20,26 @@ function App() {
       })}
     >
       <div className="container">
-        <DarkModeToggle
-          mode={mode}
-          dark="Dark"
-          light="Light"
-          size="md"
-          inactiveTrackColor="#e2e8f0"
-          inactiveTrackColorOnHover="#f8fafc"
-          inactiveTrackColorOnActive="#cbd5e1"
-          activeTrackColor="#334155"
-          activeTrackColorOnHover="#1e293b"
-          activeTrackColorOnActive="#0f172a"
-          inactiveThumbColor="#1e293b"
-          activeThumbColor="#e2e8f0"
-          onChange={(mode) => {
-            setMode(mode);
-          }}
-        />
+        <div className="toggle">
+          <DarkModeToggle
+            mode={mode}
+            dark="Dark"
+            light="Light"
+            size="sm"
+            inactiveTrackColor="#e2e8f0"
+            inactiveTrackColorOnHover="#f8fafc"
+            inactiveTrackColorOnActive="#cbd5e1"
+            activeTrackColor="#334155"
+            activeTrackColorOnHover="#1e293b"
+            activeTrackColorOnActive="#0f172a"
+            inactiveThumbColor="#1e293b"
+            activeThumbColor="#e2e8f0"
+            onChange={(mode) => {
+              setMode(mode);
+            }}
+          />
+        </div>
         <ChakraProvider>
-          {/* <Toggle /> */}
           <Header />
           <Todos />
         </ChakraProvider>
