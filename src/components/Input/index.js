1a2ea4e9c5245eaf451c7a5c389aa86c2c8c1548
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-09-05 19:38:42 +0300

    add input content and toast alert

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
-import { Input, Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 import axios from "axios";
+import "./style.css";
 
-function InputArea({ fetchData }) {
-  const [inputValue, setInputValue] = useState("Yapılacak:");
+//toast alert
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-  const fetchPost = async () => {
+function InputArea({
+  fetchTodos,
+  setIsEditing,
+  isEditing,
+  setUpdateTodo,
+  updateTodo,
+}) {
+  const [inputValue, setInputValue] = useState("Todo:");
+
+  //toast alert
+  const notify = () =>
+    toast.info("Please enter todo", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+
+  //todo ekleme
+  const postTodo = async () => {
     const { data } = await axios.post(
       `https://6311aeb7f5cba498da835aac.mockapi.io/todos`,
       {
@@ -13,31 +38,72 @@ function InputArea({ fetchData }) {
         isCompleted: false,
       }
     );
-    console.log("data.content", data);
+    console.log("data", data);
     return data;
   };
 
-  const handleClick = (e) => {
-    if (inputValue === "") {
-      alert("Lütfen todo giriniz");
+  const handleAddClick = (e) => {
+    if (inputValue === "Todo:") {
+      notify();
     } else {
       e.preventDefault();
-      fetchPost();
-      fetchData();
-      setInputValue("Yapılacak:");
+      postTodo();
+      fetchTodos();
+      setInputValue("Todo:");
     }
   };
 
+  //todo update
+  const editTodo = async () => {
+    const { data } = await axios.put(
+      `https://6311aeb7f5cba498da835aac.mockapi.io/todos/${updateTodo.id}`,
+      {
+        content: updateTodo.content,
+        isCompleted: updateTodo.isCompleted,
+        id: updateTodo.id,
+      }
+    );
+    console.log("data", data);
+    return data;
+  };
+
+  const handleUpdateClick = (id) => {
+    setIsEditing(false); //artık düzenleme yapmadığımızdan false'a çektim.
+    editTodo();
+    fetchTodos();
+  };
+
   return (
-    <div style={{display: "flex", flexDirection: "row"}}>
-      <Input
-        placeholder="todos..."
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <Button type="submit" onClick={handleClick}>
-        Ekle
-      </Button>
+    <div className="inputDiv">
+      <ToastContainer />
+      {isEditing ? (
+        <>
+          <Input
+            className="input"
+            value={updateTodo.content}
+            onChange={(e) =>
+              setUpdateTodo({ ...updateTodo, content: e.target.value })
+            }
+          />
+          <button className="addButton" onClick={() => handleUpdateClick()}>
+            Update
+          </button>
+          <button className="addButton" onClick={() => setIsEditing(false)}>
+            Cancel
+          </button>
+        </>
+      ) : (
+        <>
+          <Input
+            className="input"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className="addButton" onClick={handleAddClick}>
+            Add
+          </button>
+        </>
+      )}
     </div>
   );
 }
